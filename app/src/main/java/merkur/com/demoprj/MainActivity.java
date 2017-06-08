848edcec0bc344be50a60fc9ccdb3e4644e18d20
rahulgit09 <rahulgit09@gmail.com>
2017-06-08 12:16:17 +0530

    MAster feature 22

--- app/src/main/java/merkur/com/demoprj/MainActivity.java
+++ app/src/main/java/merkur/com/demoprj/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //feature x ,y & z added
+
+        //Master feature 22
     }
 }
